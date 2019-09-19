8228e16c68be5fc16d7c73148c1bbc4fd8f70495
Eggbertx <eggbertx@users.noreply.github.com>
2019-09-19 12:30:47 -0700

    Add some improvements for better input handling the EventEmitter class is stable but the input handler isn't in a usable state yet.

--- src/main.js
+++ src/main.js
@@ -1,84 +1,56 @@
 // EventEmitter-sphere example usage
 
-import { Thread } from 'sphere-runtime';
+import { Thread, Console } from 'sphere-runtime';
 import { EventEmitter } from 'events';
-import { InputEventEmitter } from 'inputevents';
+import { ButtonEvent, InputEventEmitter } from 'inputevents';
 const kb = Keyboard.Default;
 const font = Font.Default;
 
 let e = new EventEmitter();
 let l;
 let ie = new InputEventEmitter();
+let be = new ButtonEvent();
 let text = "";
-
-class User {
-	constructor() {
-		this.emitter = new EventEmitter();
-		this.name = "";
-	}
-
-	onDidChangeName(callback) {
-		return this.emitter.on('did-change-name', callback);
-	}
-
-	setName(name) {
-		if(name !== this.name) {
-			this.name = name;
-			this.emitter.emit('did-change-name', name)
-		}
-
-		return this.name
-	}
-
-	destroy() {
-		this.emitter.dispose()
-	}
-}
+let console = new Console();
 
 export default class EventEmitterTest extends Thread {
 	constructor() {
 		super();
 
+		console.visible = true;
 
-		let user = new User();
-		const subscription = user.onDidChangeName((name) => SSj.log(`My name is ${name}`));
+		ie.addPressEvent(Keyboard.Default, Key.Escape, Sphere.shutDown);
 		ie.addReleaseEvent(Keyboard.Default, Key.N, () => {
-			user.setName("Something");
-		});
-		// user.setName("New name");
-		// subscription.dispose();
-		/* e.addListener("keyPressed", key => {
-			SSj.log("pressed key");
-		}, false, () => {
-			return kb.getKey() != null;
-		}); */
-
-		/* let q = e.addListener("quitter", Sphere.shutDown, true, () => {
-			return kb.isPressed(Key.Escape);
-		}); */
-
-
-		ie.addPressEvent(Keyboard.Default, Key.Escape, () => {
-			Sphere.shutDown();
-		});
-		ie.addReleaseEvent(Keyboard.Default, Key.R, () => {
-			SSj.log("R key released");
-		});
-		ie.addPressEvent(Keyboard.Default, Key.Up, () => {
-			SSj.log("Moving north");
+			console.log("N pressed")
 		});
 		
+		
+		let q = e.addListener("quitter", Sphere.shutDown, true, () => {
+			return kb.isPressed(Key.Escape);
+		});
+
+
+		ie.addReleaseEvent(Keyboard.Default, Key.R, () => {
+			console.log("R key released");
+		});
+		ie.addPressEvent(Keyboard.Default, Key.Up, () => {
+			console.log("Up key pressed");
+		});
+
+		be.addGetKeyEvent(Keyboard.Default, (key) => {
+			console.log("Key pressed: " + key);
+		});
+
+		/* be.addHoldEvent(Keyboard.Default, Key.X, () => {
+			console.log("X key held via ButtonEvent#addHoldEvent");
+		});
+
+		be.addPressEvent(Keyboard.Default, Key.P, () => {
+			console.log("P key pressed via ButtonEvent#addPressEvent");
+		}); */
 	}
 
 	on_update() {
-		// SSj.log(e.events.length)
-		if(kb.isPressed(Key.R)) e.emit("pressR");
-		/* if(Sphere.now() % 32 == 0) text += Sphere.now() + ",";
-		if(Sphere.now() == 128) dispatch.pause();
-		if(Sphere.now() == 160) dispatch.resume(); */
-	}
 
-	on_render() {
-		
 	}
 }
